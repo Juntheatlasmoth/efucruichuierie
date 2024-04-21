6b82da181a186223f783321445f1ded36e75b606
Juntheatlasmoth <153424082+Juntheatlasmoth@users.noreply.github.com>
2024-04-21 17:47:44 +0700

    Ikilmlcsjhijnilsdunmzscasdjk
    jiodfacjioed co
    keyboard not detcted
    please try pressing any key to continue

---
+++ Assets/NB.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NB : MonoBehaviour
+{
+    int j = 10;
+    int a;
+                    float fractional = 3.6f;
+         double number = 3.7;
+    string text = "Hello";
+    bool isRunning = true;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("ERROR: Keyboard not detected. Forward by pressing any key. Search diagnostics with F5.");
+        print(j);
+        print("print(print");
+    }
+}
